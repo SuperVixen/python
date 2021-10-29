ae38fc37950b8cc3f58ec16891b2a4afb0ddd088
SuperVixen <SuperVixen@xaker.ru>
2021-10-29 05:56:12 +0300

    Домашнее задание №8 Все сделаны по разбору.

---
+++ dz8-1.py
@@ -0,0 +1,32 @@
+# Написать функцию email_parse(<email_address>), которая при помощи
+# регулярного выражения извлекает имя пользователя и почтовый домен из email адреса и возвращает их в виде словаря.
+# Если адрес не валиден, выбросить исключение ValueError.
+#
+# Пример:
+# >>> email_parse('someone@geekbrains.ru')
+# {'username': 'someone', 'domain': 'geekbrains.ru'}
+# >>> email_parse('someone@geekbrainsru')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   ...
+#     raise ValueError(msg)
+# ValueError: wrong email: someone@geekbrainsru
+# Примечание: подумайте о возможных ошибках в адресе и постарайтесь учесть их в регулярном выражении;
+# имеет ли смысл в данном случае использовать функцию re.compile()?
+
+import  re
+
+def email_parse(email_address):
+    re_email = re.compile(r'(?P<username>([\w]+))@(?P<domain>[^&]+\.\w+)', re.IGNORECASE)
+    if not re_email.match(email_address):
+        raise ValueError(f'wrong e-mail: {email_address}')
+    print(re_email.match(email_address).groupdict())
+
+for i in ['someone@geekbrains.ru', 'gerasim&mumu@ru', 'alexP-ravdiNN@olson.ru']:
+    try:
+        email_parse(i)
+    except ValueError as err:
+        print(err)
+
+
+    # return mail_dict
---
+++ dz8-2.py
@@ -0,0 +1,14 @@
+# Написать регулярное выражение для парсинга файла логов web-сервера из ДЗ 6 урока nginx_logs.txt
+# (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs)
+# для получения информации вида: (<remote_addr>, <request_datetime>, <request_type>, <requested_resource>, <response_code>, <response_size>),
+# например:
+#
+# raw = '188.138.60.101 - - [17/May/2015:08:05:49 +0000] "GET /downloads/product_2 HTTP/1.1" 304 0 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"'
+# parsed_raw = ('188.138.60.101', '17/May/2015:08:05:49 +0000', 'GET', '/downloads/product_2', '304', '0')
+
+import re
+check = re.compile(r'^(\b.+\b).*\[(.+)].*\"([A-Z]+) +(/.+?)\s.*?\" (\d+) (\d+) .*$|^$')
+
+with open('nginx_logs.txt', encoding='utf-8') as f:
+    for line in f:
+        print(re.findall(check, line))
