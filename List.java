7876012573ca929ddc7514d4c6d57e5eb6e41442
Петрунёв Егор Леонидович <90984218+SuperVixen@users.noreply.github.com>
2023-06-09 20:48:02 +0200

    Add files via upload

---
+++ List.java
@@ -0,0 +1,32 @@
+public class List {
+    Node head;
+    Node tail;
+
+
+    public void revert(){
+        Node currentNode = head;
+        while (currentNode != null){
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+            if (previous == null){
+                tail = currentNode;
+            }
+            if (next == null){
+                head = currentNode;  
+            }
+            currentNode = next;
+        }
+    }
+
+
+    public class Node {
+
+        int value;
+        Node next;
+        Node previous;
+
+    }
+
+}
